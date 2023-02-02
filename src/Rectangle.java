417c3ae2fc056a763244cd4cc466f4ac3964bd08
Brandon Smith <smithb@wlwv.k12.or.us>
2023-02-02 09:35:33 -0800

    accessors done with documentation

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,4 +1,14 @@
 import java.text.DecimalFormat;
+
+/**
+ * @file Rectangle.java
+ * @date 2/2/2023
+ * @author Mr. Smith
+ *
+ * @description Creates a rectangle with a width and height or a square with a side length.
+ * The user can also calculate the perimeter and area of the rectangle and
+ * print information about the Rectangle.
+ */
 public class Rectangle { //make public for CS A
 
     //instance variables
@@ -14,9 +24,24 @@ public class Rectangle { //make public for CS A
     }
 
     //overloaded constructor (with parameters)
+
+    /**
+     * Creates a Rectangle object with specified width and height. If the
+     * parameters are not greater than 0, they are set to 1.0
+     *
+     * @param w width of the rectangle
+     * @param h height of the rectangle
+     */
     public Rectangle(double w, double h){
-        width = w;
-        height = h;
+        width = 1.0;
+        height = 1.0;
+        if(w > 0){
+            width = w;
+        }
+        if(h > 0){
+            height = h;
+        }
+
     }
 
     public Rectangle(double width){
@@ -25,7 +50,32 @@ public class Rectangle { //make public for CS A
 
     //accessors
 
+    /**
+     *
+     * @return the width of the rectangle
+     */
+    public double getWidth(){
+        return width;
+    }
+
+    /**
+     *
+     * @return the height of the rectangle
+     */
+    public double getHeight(){
+        return height;
+    }
+
     //mutators
+
+    /**
+     * Allows the user to change the width of the rectangle.
+     *
+     * @param newWidth desired width
+     */
+    public void setWidth(double newWidth){
+        width = newWidth;
+    }
 
 
     //other methods
@@ -34,5 +84,14 @@ public class Rectangle { //make public for CS A
 
 
     //toString method
+
+    /**
+     *  Prints the height and width of rectangle to 2 decimal places
+     */
+    public String toString(){
+        DecimalFormat df = new DecimalFormat("#.##");
+        return "The height of the rectangle is " + df.format(height) +
+                " and the width is " + df.format(width) + ".";
+    }
 
 }
